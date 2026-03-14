a3b6baff1c3812404bbf0f24ea072d07acb56775
Wippy74 <vsevoloduzaninov@gmail.com>
2026-03-15 01:19:18 +0300

    Defined thread struct and ApiHandler class with fields and methods

--- include/api_handler.h
+++ include/api_handler.h
@@ -1,12 +1,15 @@
 #pragma once
 
 #include <nlohmann/json.hpp>
+#include "cache.h"
+#include <vector>
+#include <map>
+#include <stdexcept>
 #include <string>
 
-struct thread {
+struct Thread {
   std::string uid;
   std::string title;
-  std::string interval;
   std::string number;
   std::string short_title;
   std::string thread_method_link;
@@ -17,8 +20,18 @@ struct thread {
   std::string express_type;
   std::string from;
   std::string to;
+  double duration;
 };
 
 class ApiHandler {
-  
+public:
+  ApiHandler(const std::string& ApiKey, Cache& cache);
+
+  std::vector<Thread> search(const std::string& from, const std::string& to, const std::string& date, const std::string& transport = "");
+private:
+  std::string ApiKey_;
+  Cache& cache_;
+  nlohmann::json HttpGet(const std::string& endpoint, const std::map<std::string, std::string>& params);
+  std::string MakeCacheKey(const std::string& endpoint, const std::map<std::string, std::string>& params);
+  Thread ParseThread(const nlohmann::json& j);
 };
