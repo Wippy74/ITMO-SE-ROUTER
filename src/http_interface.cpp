4ead2dbaa43496b1d9e8b5acbd3d5cd3dbe81a3c
Wippy74 <vsevoloduzaninov@gmail.com>
2026-03-22 22:29:41 +0300

    Implemented get method with manual response

---
+++ src/http_interface.cpp
@@ -0,0 +1,15 @@
+#include "../include/http_interface.h"
+#include <cpr/cpr.h>
+
+Response HttpClient::Get(const std::string& URL, const std::map<std::string, std::string>& params, int timeout) {
+  cpr::Parameters cpr_params;
+  for (const auto& [key, value] : params) {
+    cpr_params.Add({key, value});
+  }
+  cpr::Response r = cpr::Get(cpr::Url(URL), cpr_params, cpr::Timeout(timeout));
+  Response resp;
+  resp.status_code = r.status_code;
+  resp.text = r.text;
+  resp.error_message = r.error.message;
+  return resp;
+}
