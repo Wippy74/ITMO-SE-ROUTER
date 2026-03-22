cc8dfc374b35d62b3b7e0c0c0bf0e034279c9b64
Wippy74 <vsevoloduzaninov@gmail.com>
2026-03-22 13:31:16 +0300

    Implemented PrintGuide function

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,5 +1,15 @@
 #include "../include/api_handler.h"
 #include "../include/cache.h"
-#include "../include/router.h"
+#include "../include/route_builder.h"
+#include "../include/config.h"
 
-int main() {}
+void PrintGuide(const char* p) {
+  std::cout << "Поиск маршрутов между городами с помощью API Яндекс Расписаний" << '\n\n';
+  std::cout << "Режимы работы: \n\n";
+  std::cout << "1. Одиночный запрос: \n" << p << " <откуда> <куда> <дата> <максимальное количество пересадок> \n\n";
+  std::cout << "2. Интерактивный режим: \n" << p << "(даллее вводить запросы)\n" << "Выход из интерактивного режима осуществляется командой quit";
+}
+
+int main() {
+
+}
